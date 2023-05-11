5e4c0e29f55a40ad2d92e4ee2688a249eb538892
GiorgioCitterio <giorgio.citterio@issgreppi.it>
2023-05-11 13:35:04 +0200

    tag list carica ultimi valori

--- ViewModels/TagPageViewModel.cs
+++ ViewModels/TagPageViewModel.cs
@@ -1,7 +1,10 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using MauiAuth0App.Auth0;
+using MauiAuth0App.Extensions;
 using MauiAuth0App.Models;
+using MauiAuth0App.Views;
+using System.Collections.ObjectModel;
 using System.Text.Json;
 using Device = MauiAuth0App.Models.Device;
 using Encoding = System.Text.Encoding;
@@ -11,17 +14,16 @@ namespace MauiAuth0App.ViewModels
     public partial class TagPageViewModel : ObservableObject
     {
         private readonly Device _device;
-        private bool _execute;
         private readonly string _organizationId;
-        [ObservableProperty]
-        public List<Tag> tags;
+        public ObservableCollection<Tag> Tags { get; set; }
         private HttpClient client;
         private string text;
+        private int i = 0;
+        private List<Tag> tagsList = new();
 
         public TagPageViewModel(Device dv, HttpClient client)
         {
             _device = dv;
-            _execute = true;
             _organizationId = dv.OrgResourceId;
             this.client = client;
             Tags = new();
@@ -38,148 +40,53 @@ namespace MauiAuth0App.ViewModels
                 //Tags = Tags.Where(t => t.modelPath.ToLower().Contains(text)).ToList();
             }
         }
-        
+
+        [RelayCommand]
         public async Task GetAllTags()
         {
-            Tags = await FindTagsDevice(_organizationId, _device);
+            var tagList = await FindTagsDevice();
+            foreach (var item in tagList)
+            {
+                Tags.Add(item);
+            }
         }
 
         [RelayCommand]
-        private async void SelectItem(Tag tag) {
-            if (!_execute || tag == null)
-                return;
-
-            string tagName = tag.modelPath;
-            string action = await Application.Current.MainPage.DisplayActionSheet("Cosa vuoi fare?", "Indietro", null, "Aggiungi un valore", "Visualizza i valori");
-
-            if (action == null)
-                return;
-
-            if (action == "Visualizza i valori") {
-                action = await Application.Current.MainPage.DisplayActionSheet("Cosa vuoi vedere?", "Annulla", null, "Ultimi valori", "Ultimi 15 minuti", "Ultimo giorno", "Ultimo mese", "Ultimo anno");
-                if (action != "Annulla") {
-                    _execute = false;
-                    Tags = await FoundValueTagDevice(tagName, _device, _organizationId, action);
-                }
-                return;
-            }
-
-            if (action == "Aggiungi un valore") {
-                string newTagValueString = await Application.Current.MainPage.DisplayPromptAsync("Inserisci il valore", "Inserisci il valore che il tag dovrebbe assumere");
-                if (newTagValueString == null)
-                    return;
-
-                var success = int.TryParse(newTagValueString, out int newtagValue);
-
-                if (!success) {
-                    await Application.Current.MainPage.DisplayAlert("Errore", "Il campo inserito non è valido", "Ok");
-                    return;
-                }
-
-                if (tagName == null) {
-                    await Application.Current.MainPage.DisplayAlert("Errore", "Potresti non essere connesso ", "Ok");
-                    return;
-                }
-
-                bool response = await AddDeviceTagValue(tagName, newtagValue, _device, _organizationId);
-                if (response) {
-                    await Application.Current.MainPage.DisplayAlert("Conferma", $"Hai assegnato al tag: {tagName} il valore: {newTagValueString}", "Ok");
-                } else {
-                    await Application.Current.MainPage.DisplayAlert("Errore", "Connessione a internet assente", "Ok");
-                }
-            }
+        private async void GoToTagDetailsPage(Tag tag)
+        {
+            await App.Current.MainPage.Navigation.PushAsync(new TagDetailsPage(tag));
         }
 
-        private async Task<List<Tag>> FindTagsDevice(string organizationId, Device device)
+        private async Task<List<Tag>> FindTagsDevice()
         {
             try
             {
                 string json = await TokenHandler.ExecuteWithPermissionToken(client, 
-                    async()=> await client.GetStringAsync($"https://app.corvina.io/svc/platform/api/v1/organizations/{organizationId}/devices/{device.DeviceId}/tags?modelPath=%2A%2A&since=2000-03-11T17%3A35%3A44.652Z&to=2050-01-01T00%3A00%3A00.000Z&sinceAfter=false&limit=1000&format=json&timestampFormat=unix&aggregation=%7B%22type%22%3A%22average%22%2C%22sampling%22%3A%7B%22extent%22%3A120%2C%22size%22%3A2%2C%22unit%22%3A%22minutes%22%7D%7D"));
-                List<Tag> tagsList = new();
+                    async()=> await client.GetStringAsync($"https://app.corvina.io/svc/platform/api/v1/organizations/{_organizationId}/devices/{_device.DeviceId}/tags?modelPath=%2A%2A&since=2000-03-11T17%3A35%3A44.652Z&to=2050-01-01T00%3A00%3A00.000Z&sinceAfter=false&limit=1000&format=json&timestampFormat=unix&aggregation=%7B%22type%22%3A%22average%22%2C%22sampling%22%3A%7B%22extent%22%3A120%2C%22size%22%3A2%2C%22unit%22%3A%22minutes%22%7D%7D"));
                 Tag[] deviceTag = JsonSerializer.Deserialize<Tag[]>(json);
-                foreach (var item in deviceTag)
+                int k = i + 10;
+                for (; i < k; i++)
                 {
-                    tagsList.Add(item);
+                    tagsList.Add(deviceTag[i]);
+                    string tagName = tagsList[i].modelPath;
+                    var response = await TokenHandler.ExecuteWithPermissionToken(client, async () => 
+                        await client.GetAsync($"https://app.corvina.io/svc/platform/api/v1/organizations/{_organizationId}/devices/{_device.DeviceId}/tags?modelPath={tagName}&limit=1"));
+                    string jsonTagValues = await response.Content.ReadAsStringAsync();
+                    Tag[] deviceTagValues = JsonSerializer.Deserialize<Tag[]>(jsonTagValues);
+                    if (deviceTagValues[0].data is null)
+                        continue;
+                    foreach (var item in deviceTagValues[0].data)
+                    {
+                        tagsList[i].tagValue = "Data: " + UnixTimeStampToDateTime(item[0].ToString()) + "\nValore: " + item[1];
+                    }
                 }
                 return tagsList;
-            }
-            catch
-            {
-                return null;
-            }
-        }
-
-        private async Task<bool> AddDeviceTagValue(string tagName, int tagValue, Device device, string organizationId)
-        {
-            try
-            {
-                await TokenHandler.ExecuteWithPermissionToken(client, 
-                    async ()=> await client.PostAsync($"https://app.corvina.io/svc/platform/api/v1/organizations/{organizationId}/devices/{device.DeviceId}/tags", new StringContent("{\"data\": [{\"modelPath\": \"" + tagName + "\",\"v\": " + tagValue + "}] }", Encoding.UTF8, "application/json")));
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
-        }
-
-        private async Task<List<Tag>> FoundValueTagDevice(string tagName, Device device, string organizationId, string action)
-        {
-            try
-            {
-                List<Tag> tagsList = new();
-                DateTime now = DateTime.Now.ToUniversalTime();
-                DateTime date = new();
-                if (action != "Last Values")
-                {
-                    switch (action)
-                    {
-                        case "Ultimi 15 minuti":
-                            date = now.AddMinutes(-15).ToUniversalTime();
-                            break;
-                        case "Ultimo giorno":
-                            date = now.AddDays(-1).ToUniversalTime();
-                            break;
-                        case "Ultimo mese":
-                            date = now.AddMonths(-1).ToUniversalTime();
-                            break;
-                        case "Ultimo anno":
-                            date = now.AddYears(-1).ToUniversalTime();
-                            break;
-                        default:
-                            date = now;
-                            break;
-                    }
-                    string arrivo = now.ToString("s") + ".000Z";
-                    string inizio = date.ToString("s") + ".000Z";
-                    string final = "since=" + inizio + "&to=" + arrivo;
-                    var response = await TokenHandler.ExecuteWithPermissionToken(client, 
-                        async () => await client.GetAsync($"https://app.corvina.io/svc/platform/api/v1/organizations/{organizationId}/devices/{device.DeviceId}/tags?modelPath={tagName}&limit=1000&{final}"));
-                    string json = await response.Content.ReadAsStringAsync();
-                    Tag[] deviceTag = JsonSerializer.Deserialize<Tag[]>(json);
-                    foreach (var item in deviceTag[0].data)
-                    {
-                        tagsList.Add(new Tag() { tagValue = "Data: " + UnixTimeStampToDateTime(item[0].ToString()) + "\nValore: " + item[1] });
-                    }
-                    return tagsList;
-                }
-                else
-                {
-                    var response = await TokenHandler.ExecuteWithPermissionToken(client, async () => await client.GetAsync($"https://app.corvina.io/svc/platform/api/v1/organizations/{organizationId}/devices/{device.DeviceId}/tags?modelPath={tagName}&limit=1"));
-                    string json = await response.Content.ReadAsStringAsync();
-                    Tag[] deviceTag = JsonSerializer.Deserialize<Tag[]>(json);
-                    foreach (var item in deviceTag[0].data)
-                    {
-                        //TODO: cambiare convertitore
-                        tagsList.Add(new Tag() { tagValue = "Data: " + UnixTimeStampToDateTime(item[0].ToString()) + "\nValore: " + item[1] });
-                    }
-                    return tagsList; 
-                }
+                
             }
             catch (Exception ex)
             {
-                return null;
+                await App.Current.MainPage.DisplayAlert("Error", ex.Message, "OK");
+                return new List<Tag>();
             }
         }
 
