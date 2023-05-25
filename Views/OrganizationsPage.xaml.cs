f614ce0944360ca1985343a8639d417b7001ddb3
GiorgioCitterio <giorgio.citterio@issgreppi.it>
2023-05-25 12:36:47 +0200

    organization pagina device si apre una sola volta

--- Views/OrganizationsPage.xaml.cs
+++ Views/OrganizationsPage.xaml.cs
@@ -12,14 +12,13 @@ namespace MauiAuth0App.Views;
 
 public partial class OrganizationsPage : ContentPage
 {
-
     private OrganizazionsPageViewModel model;
 	private readonly HttpClient client;
+    private bool isBusy = false;
 
 	public OrganizationsPage(HttpClient client) {
 		InitializeComponent();
 		this.client = client;
-
         model = new OrganizazionsPageViewModel(client);
         BindingContext = model;
     }
@@ -30,11 +29,15 @@ public partial class OrganizationsPage : ContentPage
     }
 
     private async void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e) {
+        if (isBusy)
+            return;
+        isBusy = true;
         var organization = (sender as BindableObject).BindingContext as Organization;
         var page = new OptionsPage(client);
         var model = new OptionsPageViewModel(client, organization);
         await model.LoadViewModel();
         page.BindingContext = model;
         await Navigation.PushAsync(page);
+        isBusy = false;
     }
 }
